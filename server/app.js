23f6b8537c7ea1e88c99eab3fe9f6b258e0d6374
AjiNotoKusumo <aji201101@gmail.com>
2026-02-25 13:41:05 +0700

    Update add socket capability

--- server/app.js
+++ server/app.js
@@ -18,11 +18,13 @@ io.on("connection", async (socket) => {
     socket.on("create-room", (roomData) => {
         const roomCode = Math.random().toString(36).substring(2, 7).toUpperCase()
         roomData.roomCode = roomCode
-
+        
         activeRooms[roomCode] = {
             roomName: roomData.roomName,
             maxPlayer: roomData.maxPlayer,
-            players: []
+            category: roomData.category,
+            hostname: roomData.hostName,
+            players: [roomData.hostName]
         }
 
         socket.join(roomCode)
@@ -33,36 +35,51 @@ io.on("connection", async (socket) => {
 
     socket.on("get-active-room", (roomCode) => {
 
-        if(activeRooms[roomCode]) {
-            socket.join(roomCode)
-            socket.emit("active-room", activeRooms[roomCode])
-        } else {
+        if(!activeRooms[roomCode]) {
             socket.emit("active-room-not-found", 'Room not found')
+            return
         }
+
+        socket.join(roomCode)
+        socket.emit("active-room", activeRooms[roomCode])
     })
 
 
     socket.on("join-room", (playerData) => {
         const {roomCode, name} = playerData
         const room = activeRooms[roomCode]
-         
-        if(room){   
-            if(activeRooms[roomCode].players.length < activeRooms[roomCode].maxPlayer) {
-                
-                activeRooms[roomCode].players.push(name)
-                socket.join(roomCode)
-                console.log(
-                "ROOM MEMBERS:",
-                [...(io.sockets.adapter.rooms.get(roomCode) || [])]
-                )
-                socket.emit("joined-room", {roomCode, activeRoom: activeRooms[roomCode]})
-                io.to(roomCode).emit("player-joined", name)
-            } else {
-                socket.emit("room-full", 'Room is full')
-            }
-        } else {
+        
+        if(!room){
             socket.emit("active-room-not-found", 'Room not found')
+            return
         }
+    
+        if(activeRooms[roomCode].players.length === Number(activeRooms[roomCode].maxPlayer)) {
+            socket.emit("room-full", 'Room is full')
+            return
+        }
+            
+        activeRooms[roomCode].players.push(name)
+
+        socket.join(roomCode)
+
+        console.log("Active room:", activeRooms[roomCode]);
+        socket.emit("joined-room", {roomCode, activeRoom: activeRooms[roomCode]})
+        io.to(roomCode).emit("player-joined", name)
+        
+    })
+
+
+    socket.on("start-quiz", (roomCode) => {
+        io.to(roomCode).emit("quiz-started")
+    })
+
+    socket.on("message-new", (messageData) => {
+        console.log(`${socket.handshake?.auth?.name} send message: ${messageData.message} to room ${messageData.roomCode}`)   
+        io.to(messageData.roomCode).emit("message-received", {
+            from: messageData.name,
+            message: messageData.message,
+        })
     })
 })
 
