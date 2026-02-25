ee903ea5101870187ea009226c13f4b7c61c7fff
AjiNotoKusumo <aji201101@gmail.com>
2026-02-25 09:26:41 +0700

    Update server template

--- server/app.js
+++ server/app.js
@@ -5,12 +5,67 @@ const { Server } = require("socket.io");
 const port = 3000
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer, { 
+    cors: {
+        origin: "*",
+    }
+});
 
-io.on("connection", (socket) => {
-  // ...
+const activeRooms = {};
+
+io.on("connection", async (socket) => {
+    
+    socket.on("create-room", (roomData) => {
+        const roomCode = Math.random().toString(36).substring(2, 7).toUpperCase()
+        roomData.roomCode = roomCode
+
+        activeRooms[roomCode] = {
+            roomName: roomData.roomName,
+            maxPlayer: roomData.maxPlayer,
+            players: []
+        }
+
+        socket.join(roomCode)
+
+        socket.emit("room-created", {roomCode, activeRoom: activeRooms[roomCode]})
+    })
+
+
+    socket.on("get-active-room", (roomCode) => {
+
+        if(activeRooms[roomCode]) {
+            socket.join(roomCode)
+            socket.emit("active-room", activeRooms[roomCode])
+        } else {
+            socket.emit("active-room-not-found", 'Room not found')
+        }
+    })
+
+
+    socket.on("join-room", (playerData) => {
+        const {roomCode, name} = playerData
+        const room = activeRooms[roomCode]
+         
+        if(room){   
+            if(activeRooms[roomCode].players.length < activeRooms[roomCode].maxPlayer) {
+                
+                activeRooms[roomCode].players.push(name)
+                socket.join(roomCode)
+                console.log(
+                "ROOM MEMBERS:",
+                [...(io.sockets.adapter.rooms.get(roomCode) || [])]
+                )
+                socket.emit("joined-room", {roomCode, activeRoom: activeRooms[roomCode]})
+                io.to(roomCode).emit("player-joined", name)
+            } else {
+                socket.emit("room-full", 'Room is full')
+            }
+        } else {
+            socket.emit("active-room-not-found", 'Room not found')
+        }
+    })
 })
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
     console.log(`Website is running on port ${port}`);
 })
